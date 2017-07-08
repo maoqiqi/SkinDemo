3e05ab74e923b191ecba70e6fcbf0c2c3872990f
maoqiqi <15221394017@163.com>
2017-07-08 23:43:51 +0800

    编写'skin-demo',创建测试页面

--- skin-demo/src/main/java/com/software/march/skin/demo/MainActivity.java
+++ skin-demo/src/main/java/com/software/march/skin/demo/MainActivity.java
@@ -1,13 +1,60 @@
 package com.software.march.skin.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.et_user_name)
+    EditText etUserName;
+    @BindView(R.id.et_password)
+    EditText etPassword;
+    @BindView(R.id.btn_login)
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        enableLoginBtn();
+        etUserName.addTextChangedListener(textWatcher);
+        etPassword.addTextChangedListener(textWatcher);
+    }
+
+    private TextWatcher textWatcher = new TextWatcher() {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            enableLoginBtn();
+        }
+    };
+
+    private void enableLoginBtn() {
+        if (etUserName.getText().length() != 0 && etPassword.getText().length() != 0) {
+            btnLogin.setAlpha(1.0f);
+            btnLogin.setEnabled(true);
+        } else {
+            btnLogin.setAlpha(0.3f);
+            btnLogin.setEnabled(false);
+        }
     }
 }
